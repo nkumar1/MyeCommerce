73fdcec7608e94b558263c48c5e9138e2a52700a
Nishant Kumar <nishant.kr@hotmail.com>
2025-05-21 12:49:48 +0530

    Fix DI issue.

--- OrderPlaceService/Program.cs
+++ OrderPlaceService/Program.cs
@@ -1,3 +1,6 @@
+﻿using ECommerce.Shared.Interface;
+using ECommerce.Shared;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Register Kafka producer
+builder.Services.AddSingleton<IKafkaProducer, KafkaProducer>();
 
 var app = builder.Build();
 
