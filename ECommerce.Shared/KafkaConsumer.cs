43a614655a36e1dcded5d89b5527081076399a7f
Nishant Kumar <nishant.kr@hotmail.com>
2025-05-21 21:29:26 +0530

    Update kafka offset configuration.

--- ECommerce.Shared/KafkaConsumer.cs
+++ ECommerce.Shared/KafkaConsumer.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace ECommerce.Shared
 {
-    public class KafkaConsumer: IKafkaConsumer
+    public class KafkaConsumer: IKafkaConsumer, IDisposable
     {
         private readonly IConsumer<string, string> _consumer;
 
@@ -19,6 +19,7 @@ namespace ECommerce.Shared
             {
                 BootstrapServers = "localhost:9092",
                 GroupId = groupId,
+                EnableAutoCommit = false,
                 AutoOffsetReset = AutoOffsetReset.Earliest
             };
             _consumer = new ConsumerBuilder<string, string>(config).Build();
@@ -29,10 +30,33 @@ namespace ECommerce.Shared
             _consumer.Subscribe(topic);
             while (!cancellationToken.IsCancellationRequested)
             {
-                var cr = _consumer.Consume(cancellationToken);
-                var data = JsonSerializer.Deserialize<T>(cr.Message.Value);
-                await onMessage(data);
+                try
+                {
+                    var cr = _consumer.Consume(cancellationToken);
+                    if (cr?.Message == null) continue;
+
+                    var data = JsonSerializer.Deserialize<T>(cr.Message.Value);
+                    await onMessage(data); //Your processing logic
+
+                    //Manually commit the offset only after successful processing
+                    _consumer.Commit(cr);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error consuming Kafka message: {ex.Message}");
+                    // No commit here — Kafka will retry the same message
+                }
+                //var cr = _consumer.Consume(cancellationToken);
+                //var data = JsonSerializer.Deserialize<T>(cr.Message.Value);
+                //await onMessage(data);
+                //_consumer.Commit(cr);  // ✅ Manually commit the offset
+
             }
+        }
+        public void Dispose()
+        {
+            _consumer?.Close();
+            _consumer?.Dispose();
         }
     }
 }
